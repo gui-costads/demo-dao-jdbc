5096e34dc89f4ff80f4851362d703d31f41544a6
Guilherme Costa <guilhermecosta.ds1@gmail.com>
2023-02-21 21:21:19 -0300

    Add departament class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import org.example.model.entities.Departament;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       
     }
 }
