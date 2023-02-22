4f4b0bb17e0220079dfa15304d833758c629d4bc
Guilherme Costa <guilhermecosta.ds1@gmail.com>
2023-02-22 10:29:48 -0300

    Add Seller class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
 
 import org.example.model.entities.Departament;
+import org.example.model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-       
+
     }
 }
